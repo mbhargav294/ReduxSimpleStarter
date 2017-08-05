61687d8f8552c4913dab4141b23fbffb3005bf02
Sai Madhu Bhargav <mbhargav294@gmail.com>
2017-08-05 12:39:24 -0400

    Full YouTube Project completed.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -13,7 +14,11 @@ class App extends Component {
       videos: [],
       selectedVideo: null
     };
-    YTSearch({key: API_KEY, term: 'React/Redux'}, (videos) => {
+    this.videoSearch('React native');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
@@ -22,9 +27,10 @@ class App extends Component {
   }
 
   render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
     return (
         <div>
-            <SearchBar />
+            <SearchBar onSearchTermChange={videoSearch}/>
             <VideoDetail video={this.state.selectedVideo}/>
             <VideoList
               onVideoClick={selectedVideo => this.setState({selectedVideo})}
